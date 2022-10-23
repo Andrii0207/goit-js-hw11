9190dca2b3d6c09167382a8365c8058c1ab8bd7d
Andrii Popov <popov.andrey80@gmail.com>
2022-10-23 23:05:09 +0300

    start task

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('.searh-input'),
+  searchBtn: document.querySelector('.search-btn'),
+};
+
+refs.searchBtn.addEventListener('click', onSearchImg);
+
+function onSearchImg() {
+  const KEY = '30810402-d2272724878c47174b870ed5b';
+  const BASE_URL = 'https://pixabay.com/api/';
+  const URL = `${BASE_URL}`;
+
+  fetch('URL');
+}
